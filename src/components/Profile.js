baeccf835fbd179fca6b9c0a73275ea7e970f3b8
eliu0120 <48814349+eliu0120@users.noreply.github.com>
2023-03-14 12:38:06 -0700

    Add files via upload
    Updated: changes from branches
    Tl;dr:
    The following pages have been linked together:
    Login
    Signup
    Firebase
    Game
    Score saving
    Death page
    Profile
    
    Additionally a test.js page is being used temporarily as a home page

---
+++ src/components/Profile.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import LineChart from './LineChart.js';
+import ProfileInfo from './ProfileInfo.js';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+function Profile() {
+	const photoURL = "https://images.squarespace-cdn.com/content/v1/570c331be321403a73df5d4e/1606041549628-TK1D7ZR4518J46ZN184Y/Space+Invader+logo.png?format=1000w";
+
+	return (<div>
+		<Box
+			component="main"
+			sx={{
+				backgroundColor: (theme) =>
+					theme.palette.mode === 'dark'
+						? theme.palette.grey[80]
+						: theme.palette.grey[900],
+				flexGrow: 1,
+				height: '100vh',
+				overflow: 'auto',
+			}}
+		>
+
+			<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+				<Grid container spacing={3}>
+					<Grid item xs={4}>
+						<Paper
+							sx={{
+								p: 2,
+								display: 'flex',
+								flexDirection: 'column',
+								height: 240,
+							}}
+						>
+							{/* Profile Picture */}
+							<center><img src={photoURL} alt="Profile" width="180" height="180" /></center>
+						</Paper>
+					</Grid>
+
+					{/* Profile Name and Info */}
+					<Grid item xs={8}>
+						<Paper
+							sx={{
+								p: 2,
+								display: 'flex',
+								flexDirection: 'column',
+								height: 240,
+								alignItems: "center",
+								textAlign: "center",
+							}}
+						>
+							<center><ProfileInfo /></center>
+						</Paper>
+					</Grid>
+
+					{/* Chart of Weight */}
+					<Grid item xs={12}>
+						<Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+							<LineChart />
+						</Paper>
+					</Grid>
+				</Grid>
+
+			</Container>
+
+		</Box>
+	</div>);
+}
+
+export default Profile;
+
+
+
+
+
---
+++ src/components/ProfileInfo.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import Typography from '@mui/material/Typography';
+import { auth, db } from "../firebase.js";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+
+function ProfileInfo() {
+
+	const defaultData = {
+		"username": "John Doe",
+		"email": "johndoe@somecompany.com",
+	};
+
+	const [data, setData] = useState(defaultData);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, async (user) => {
+			if (user) {
+				let info = {};
+				const usersRef = collection(db, "users");
+				const q = query(usersRef, where("uid", "==", user.uid));
+
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach((doc) => {
+					info = doc.data();
+				});
+				setData(info);
+			}
+		});
+	}, []);
+
+	return (
+		<Typography variant="p" component="p">
+			<center><font size="20"><b>{data.username}</b></font></center>
+			<br /><br />
+			<center>{data.email}</center>
+			<br /><br />
+			<center><em>Who's record will you beat?</em></center>
+		</Typography>
+	);
+}
+
+export default ProfileInfo;
