82fb3ba83700fcd108d1563fac5fb02ff657cabe
Justin Bou <bou22716@gmail.com>
2023-03-14 12:21:33 -0700

    -added button for profile page

--- homepage.js
+++ homepage.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from "react";
 import {Stack ,Button, Typography} from "@mui/material";
+import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
 import RocketLaunchRoundedIcon from '@mui/icons-material/RocketLaunchRounded';
 import background from "./background.jpg";
 
@@ -16,6 +17,9 @@ function App() {
       <Typography fontSize={100} color="white" style= {{minHeight:"40vh"}} margin="normal" textAlign="center">Space Invaders</Typography>
       <Stack>
       <Button endIcon = {<RocketLaunchRoundedIcon/>} sx= {{ borderRadius:3}} margin="normal" color= "success"varient="contained" >Play </Button>
+      </Stack>
+      <Stack>
+      <Button endIcon={<AccountBoxRoundedIcon/>} sx= {{marginTop:3, borderRadius:3}} margin="normal" color="secondary" varient="contained" >Profile</Button>
       </Stack>
     </div>
     </div>
