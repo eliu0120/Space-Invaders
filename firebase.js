a3762ad56d7a055001659c5bf07d444413767ad2
Krishi Sabarwal <krishi@s-128-97-122-10.resnet.ucla.edu>
2023-03-13 18:33:17 -0700

    Set up Firebase
    Set up Firebase project and linked it
    with firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+
+import {
+    getAuth, signInWithEmailAndPassword, onAuthStateChanged,
+    createUserWithEmailAndPassword, sendPasswordResetEmail, signOut,
+} from 'firebase/auth';
+import {
+    getFirestore, collection, addDoc, setDoc, doc,
+} from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYqpIU5GgE6xS4RINuQ_GHzK_6G-J0Hi8",
+    authDomain: "space-invaders-6b7d9.firebaseapp.com",
+    databaseURL: "https://space-invaders-6b7d9-default-rtdb.firebaseio.com/",
+    projectId: "space-invaders-6b7d9",
+    storageBucket: "space-invaders-6b7d9.appspot.com",
+    messagingSenderId: "731300708466",
+    appId: "1:731300708466:web:13185967770ed090f582eb"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
