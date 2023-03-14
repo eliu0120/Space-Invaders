aea17042ae197a1856903f280d6fe1d25f78584a
Justin Bou <bou22716@gmail.com>
2023-03-14 13:10:29 -0700

    -add home page -add start button to link to game

---
+++ home.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import React, { useState } from "react";
+import {Stack ,Button, Typography} from "@mui/material";
+import RocketLaunchRoundedIcon from '@mui/icons-material/RocketLaunchRounded';
+import background from "./background.jpg";
+
+
+function App() {
+  return (
+    <div  style={{height: '100vh',
+      width: '162.91vh',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      position: 'aboslute', backgroundImage: `url(${background})`}} >
+    <div className="App" sx={{":hover":{boxShadow:"10px 10px 20px #ccc",}}}>
+      <Typography fontSize={100} color="white" style= {{minHeight:"40vh"}} margin="normal" textAlign="center">Space Invaders</Typography>
+      <Stack>
+      <Button endIcon = {<RocketLaunchRoundedIcon/>} sx= {{ borderRadius:3}} margin="normal" color= "success"varient="contained" >Play </Button>
+      </Stack>
+    </div>
+    </div>
+  );
+}
+
+export default App;
