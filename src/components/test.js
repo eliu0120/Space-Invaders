349aaf7e91b43a07a0d953ba80c470e814fab103
Eric Liu <eliu0120@gmail.com>
2023-03-14 21:14:57 -0700

    Made home page beautiful + removed misc files

--- src/components/test.js
+++ src/components/test.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { auth, logout } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
+import RocketLaunchRoundedIcon from '@mui/icons-material/RocketLaunchRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import {Stack ,Button, Typography} from "@mui/material";
 
 export default function Test() {
@@ -25,20 +28,23 @@ export default function Test() {
       width: '100vw',
       height: '100vh'
     }}>
-      <div className="App" sx={{ ":hover": { boxShadow: "10px 10px 20px #ccc", } }}>
-        <Typography fontSize={100} color="white" style={{ minHeight: "40vh" }} margin="normal" textAlign="center">Space Invaders</Typography>
-        <Stack>
-          <Button sx={{ borderRadius: 3 }} margin="normal" color="success" varient="contained" onClick = { () => {
+      <div className="App" sx={{":hover":{boxShadow:"5px 5px 20px #ccc",}}}>
+        <Typography fontSize={100} color="white" style= {{minHeight:"40vh"}} fontFamily="Ultra" margin="normal" textAlign="center">Space Invaders</Typography>
+        <Stack justifyContent="center" alignItems="center" >
+          <Button style={{maxWidth:"150px"}} textAlign='center' endIcon = {<RocketLaunchRoundedIcon/>} sx= {{ borderRadius:3}} margin="normal" 
+          color= "success"variant="contained" onClick = { () => {
             window.location.href ="game.html"
           }}> Play </Button>
         </Stack>
-        <Stack>
-          <Button sx={{ marginTop: 3, borderRadius: 3 }} margin="normal" color="secondary" varient="contained" onClick = { () => {
+        <Stack justifyContent="center" alignItems="center" >
+          <Button style={{maxWidth:"150px"}}  textAlign='center' endIcon={<AccountBoxRoundedIcon/>} sx= {{marginTop:3, borderRadius:3}} 
+          margin="normal" color="secondary" variant="contained" onClick = { () => {
             navigate("/profile");
           }}>Profile</Button>
         </Stack>
-        <Stack>
-          <Button sx={{ marginTop: 3, borderRadius: 3, minHeight: "5vh" }} width={"200%"} color="error" varient="contained" onClick = { () => {
+        <Stack justifyContent="center" alignItems="center" >
+          <Button style={{maxWidth:"150px"}}  textAlign='center' endIcon={<LogoutRoundedIcon/>} 
+          sx= {{ marginTop:3, borderRadius:3, minHeight: "5vh"}} width={"200%"} color="error" variant="contained" onClick = { () => {
             logout();
             navigate("/");
           }}>Logout</Button>
